ee269bdcfee916e97a1167d46d5c37e16dcf15c3
zerok-cell <swiftkey382@gmail.com>
2025-09-19 18:42:39 +0300

    bug: to fix bad syntax in laptop host

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -3,19 +3,17 @@ let
 
   nixosSystem = import ../../utils/nixosSystem.nix { inherit inputs; };
   gAttr = import ../general-attr.nix;
-in {
 
-  laptop = nixosSystem {
-    system = "x86_64-linux";
-    specialArgs = { inherit inputs; };
-    modules = [
-      inputs.home-manager.nixosModules.home-manager
-      {
-        home-manager.users.zerok.imports =
-          [ ../../home-manager ./output-sway-rewrite.nix ];
-      }
-      gAttr.home-manager-settings
-      ./hardware-configuration.nix
-    ] ++ gAttr.general-path;
-  };
+in nixosSystem {
+  system = "x86_64-linux";
+  specialArgs = { inherit inputs; };
+  modules = [
+    inputs.home-manager.nixosModules.home-manager
+    {
+      home-manager.users.zerok.imports =
+        [ ../../home-manager ./output-sway-rewrite.nix ];
+    }
+    gAttr.home-manager-settings
+    ./hardware-configuration.nix
+  ] ++ gAttr.general-path;
 }
