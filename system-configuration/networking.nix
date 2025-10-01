d7f2af4ae88855cab8cc082a399559465337a9e8
zerokqx <swiftkey382@gmail.com>
2025-10-01 18:41:43 +0300

    new terworking settings

--- system-configuration/networking.nix
+++ system-configuration/networking.nix
@@ -4,7 +4,8 @@
     nameservers = [ "8.8.8.8" ];
     hostName = "nixos";
     networkmanager.enable = false;
-
+    firewall.allowedTCPPorts = [ 5173 ]; # Разрешить порт 5173
+    firewall.enable = true;
     wireless.iwd.enable = true;
   };
 }
