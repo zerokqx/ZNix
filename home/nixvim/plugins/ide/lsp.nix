3051f6a90d9ad7fddd46e9b4c5795bb742c275d6
zerokqx <swtiftkey382@gmail.com>
2026-02-04 18:00:24 +0300

    Fix LSP attach hook

--- home/nixvim/plugins/ide/lsp.nix
+++ home/nixvim/plugins/ide/lsp.nix
@@ -12,27 +12,6 @@
           vim.lsp.protocol.make_client_capabilities()
         )
       '';
-      onAttach = lib.nixvim.mkRaw ''
-        function(client, bufnr)
-          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-          local is_large = ok and stats and stats.size and stats.size > 256 * 1024
-
-          local disable_format = {
-            tsserver = true,
-            vtsls = true,
-            eslint = true,
-          }
-
-          if disable_format[client.name] or is_large then
-            client.server_capabilities.documentFormattingProvider = false
-            client.server_capabilities.documentRangeFormattingProvider = false
-          end
-
-          if is_large and client.server_capabilities.semanticTokensProvider then
-            client.server_capabilities.semanticTokensProvider = nil
-          end
-        end
-      '';
       servers = {
         prismals = {
           enable = true;
@@ -120,5 +99,33 @@
         update_in_insert = true,
       }
     )
+
+    local function nixvim_on_attach(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      local bufnr = args.buf
+      if not client then return end
+
+      local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+      local is_large = ok and stats and stats.size and stats.size > 256 * 1024
+
+      local disable_format = {
+        tsserver = true,
+        vtsls = true,
+        eslint = true,
+      }
+
+      if disable_format[client.name] or is_large then
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end
+
+      if is_large and client.server_capabilities.semanticTokensProvider then
+        client.server_capabilities.semanticTokensProvider = nil
+      end
+    end
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = nixvim_on_attach,
+    })
   '';
 }
